1088fb54ab3627bbceb3e34eb0d2ce5f7d5e76e8
AnaTherezaMotta <65545245+AnaTherezaMotta@users.noreply.github.com>
2020-07-16 17:56:15 -0300

    appPlaneta
    aulaIOS

---
+++ aula_pratica/PlanetaCell.swift
@@ -0,0 +1,16 @@
+//
+//  PlanetaCell.swift
+//  aula_pratica
+//
+//  Created by COTEMIG on 29/06/20.
+//  Copyright © 2020 Cotemig. All rights reserved.
+//
+
+import UIKit
+
+class PlanetaCell: UITableView{
+    @IBOutlet weak var imagemPlaneta: UIImageView!
+    @IBOutlet weak var nomePlaneta: UILabel!
+    @IBOutlet weak var descricaoPlaneta: UILabel!
+    
+}
